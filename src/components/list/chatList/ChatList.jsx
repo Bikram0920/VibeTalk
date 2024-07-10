15e4aaa96700538051d60abd8b5cd926811235d0
Bikram Dangua <bikramdangua5@gmail.com>
2024-07-10 16:19:57 +0530

    New chat

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./chatList.css"
 import AddUser from './addUser/AddUser';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../library/firebase';
+import { useUserStore } from '../../library/userStore';
 
 function ChatList() {
+  const [chats,setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
+
+  const {currentUser} = useUserStore();
+
+  useEffect(() => {
+
+    if (!currentUser?.id) return;
+
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
+      const items = res.data().chats;
+      // console.log(items)
+      const promises = items.map(async (item) =>{
+        const userDocRef = doc(db, "users", item.recieverId);
+        const userDocSnap = await getDoc(userDocRef);
+
+        const user = userDocSnap.data();
+        // console.log(user)
+        return {...item,user}
+      })
+      const chatData = await Promise.all(promises);
+        console.log(chatData);
+        setChats(chatData.sort((a,b) => b.updatedAt - a.updatedAt))
+    });
+
+    return () =>{
+      unSub();
+    }
+  }, [currentUser?.id])
+
   return (
     <div className='chatList' >
       <div className="search">
@@ -13,48 +45,15 @@ function ChatList() {
         </div>
         <img src={ addMode ? "./minus.png" : "./plus.png"} alt="" className='plus' onClick={() => setAddMode((prev) => !prev) }/>
       </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Bikram</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Bikram</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Bikram</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Bikram</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Bikram</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Bikram</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {chats.map((chat) => (
+          <div className="item" key={chat.chatId}>
+          <img src={chat.user.avatar || "./avatar.png"} alt="" />
+          <div className="texts">
+            <span>{chat.user.username}</span>
+            <p>{chat.lastMessage}</p>
+          </div>
+        </div> 
+      ))}
       {addMode && <AddUser/>}
     </div>
   )
--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,20 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./addUser.css"
+import {db} from "../../../library/firebase"
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore"
+import { useUserStore } from '../../../library/userStore';
 
 function AddUser() {
+  
+  const [user,setUser] = useState(null);
+
+  const {currentUser} = useUserStore();
+
+  const handleSearch = async (e) =>{
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get('userName');
+
+    try {
+      const userRef = collection(db, "users");
+
+      // Create a query against the collection.
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data())
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleAdd = async (e) =>{
+    
+    const chatRef = collection(db,"chats")
+    const userChatRef = collection(db,"userchats")
+
+    try {
+      const newChatRef = doc(chatRef)
+
+      await setDoc(newChatRef,{
+        createdAt: serverTimestamp(),
+        messages:[]
+      });
+      
+      await updateDoc(doc(userChatRef, user.id),{
+        chats:arrayUnion({
+          chatId:newChatRef.id,
+          lastMessage:"",
+          recieverId:currentUser.id,
+          updatedAt: Date.now(),
+        })
+      })
+
+      await updateDoc(doc(userChatRef, currentUser.id),{
+        chats:arrayUnion({
+          chatId:newChatRef.id,
+          lastMessage:"",
+          recieverId:user.id,
+          updatedAt: Date.now(),
+        })
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='addUser' >
-      <form action="">
+      <form onSubmit={handleSearch} >
         <input type="text" name='userName' placeholder='UserName' />
         <button>Search</button>
       </form>
-      <div className="user">
+      {user && <div className="user">
         <div className='details'>
-          <img src="avatar.png" alt="" />
-          <span>Bikram</span>
+          <img src={user.avatar || "avatar.png"} alt="" />
+          <span>{user.username}</span>
         </div>
-        <button>Add User</button>
-      </div>
+        <button onClick={handleAdd} >Add User</button>
+      </div>}
     </div>
   )
 }
