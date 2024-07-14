d8b83294cf5883c480b0e5e7be408a665fe6f413
Bikram Dangua <bikramdangua5@gmail.com>
2024-07-14 21:33:46 +0530

    Image download button

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./detail.css"
 import { signOut } from 'firebase/auth'
 import { auth, db } from '../library/firebase'
 import { useChatStore } from '../library/chatStore'
-import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import { arrayUnion, doc, onSnapshot, updateDoc } from 'firebase/firestore'
 import { useUserStore } from '../library/userStore'
 
 function Detail() {
-
+  const [chat, setChat] = useState({ messages: [] });
   const {user,chatId,isReceiverBlocked,isCurrentUserBlocked,changeBlock} = useChatStore();
   const {currentUser} = useUserStore();
+  const [img, setImg] = useState(false);
+
+  const handleImg = (e) =>{
+    setImg(!img);
+  }
+
 
   const handleBlock = async () =>{
 
@@ -24,8 +30,34 @@ function Detail() {
     } catch (error) {
       console.log(error)
     }
-
   }
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, 'chats', chatId), (res) => {
+      setChat(res.data() || { messages: [] });
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [chatId]);
+
+  // console.log(chat)
+
+  const imageMessages = chat.messages.filter(message => message.hasOwnProperty('img'));
+
+  // imageMessages.map((message) => {
+  //   console.log(message.img)
+  // })
+
+  const handleDownload = (url, name) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = name;
+    document.body.appendChild(link); // Append to the body to work in Firefox
+    link.click();
+    document.body.removeChild(link); // Clean up by removing the link
+  };
 
 
   return (
@@ -47,47 +79,30 @@ function Detail() {
           </div>
           <div className="item"> 
             <span>Shared Photos</span>
-            <img src="./arrowUp.png" alt="" />
+            <img src={img ? "./arrowDown.png" : "./arrowUp.png"} alt="" onClick={handleImg}/>
           </div>
-          <div className="photos">
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="/karsten-wurth-0w-uTa0Xz7w-unsplash.jpg" alt="" />
-                <span>photo_2024_2.png</span>
+          {img && <div className="photos">
+            {(imageMessages.length > 0) && imageMessages.map((message,index) => (
+                <div className="photoItem" key={index}>
+                  <div className="photoDetail" >
+                    <img src={message.img} alt="" />
+                    <span>{message.text}</span>
+                  </div>
+                    <img src="./download.png" alt="" onClick={() => handleDownload(message.img,message.text)}/>
               </div>
-                <img src="./download.png" alt="" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="/karsten-wurth-0w-uTa0Xz7w-unsplash.jpg" alt="" />
-                <span>photo_2024_2.png</span>
-              </div>
-                <img src="./download.png" alt="" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="/karsten-wurth-0w-uTa0Xz7w-unsplash.jpg" alt="" />
-                <span>photo_2024_2.png</span>
-              </div>
-                <img src="./download.png" alt="" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="/karsten-wurth-0w-uTa0Xz7w-unsplash.jpg" alt="" />
-                <span>photo_2024_2.png</span>
-              </div>
-                <img src="./download.png" alt="" />
-            </div>
-          </div>
+            ))}
+          </div>}
           <div className="item"> 
             <span>Shared Files</span>
             <img src="./arrowUp.png" alt="" />
           </div>
         </div>
+        <div className="buttons">
         <button onClick={handleBlock}>
           {isCurrentUserBlocked ? "You are Blocked" : isReceiverBlocked ? "User Blocked" : "Block User"}
         </button>
         <button className='logout' onClick={() => auth.signOut()} >Logout</button>
+        </div>
       </div>
     </div>
   )
